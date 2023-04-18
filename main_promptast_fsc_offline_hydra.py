a60afeb06cfdcd976c68c8251e09a27743e7b716
Ste <stefano.sc.ciapponi@gmail.com>
2023-04-18 19:24:01 +0200

    Hydra Added

--- main_promptast_fsc_offline_hydra.py
+++ main_promptast_fsc_offline_hydra.py
@@ -5,34 +5,25 @@ from torch.optim import Adam, AdamW
 from Speech_CLscenario.fluentspeech import FluentSpeech
 from Speech_CLscenario.slurp_aug import Slurp
 from Speech_CLscenario.class_incremental import ClassIncremental
-# from continuum import ClassIncremental
-# from continuum.datasets import FluentSpeech
 import torch
 import torch.nn.functional as F
-import argparse
+
 from continuum.metrics import Logger
 import numpy as np
-#from model_slurp import CL_model
-#from model_transformer import CL_model, ModelDimensions
-#from model_trans_wav2vec import Seq2SeqTransformer, ModelDimensions
-#from model_confenc_wav import Seq2SeqTransformer, ModelDimensions
+
 from model_prompt import Seq2SeqTransformer, ModelDimensions
 from tools.utils import get_kdloss,get_kdloss_onlyrehe, freeze_parameters
 import time
 import datetime
-import json
 import wandb
-#from continuum import rehearsal
 from Speech_CLscenario.memory import RehearsalMemory
 from statistics import mean
 import math
-#from tools.utils import TextTransform
 from torchaudio import transforms as tr
 import warnings
 warnings.simplefilter("ignore", UserWarning)
 import os
 import torch.nn as nn
-#import pytorch_warmup as warmup
 import string
 from torchmetrics import WordErrorRate, CharErrorRate
 from torch.optim.lr_scheduler import LambdaLR
@@ -46,7 +37,7 @@ from transformers import AutoProcessor, ASTModel, AutoFeatureExtractor
 from PromptASTClassifier import PromptASTClassifier
 from prompt import Prompt, PromptArgs
 
-
+import hydra
 
 
 prompt_args = PromptArgs(length=5, 
@@ -107,115 +98,8 @@ class NoamOpt:
             min(step ** (-0.5), step * self.warmup ** (-1.5)))
 
 
-# ARGS PARSER
-def get_args_parser():
-    parser = argparse.ArgumentParser('CiCL for Spoken Language Understandig (Intent classification) on FSC: train and evaluation',
-                                     add_help=False)
-    
-    # Dataset parameters.
-    
-    parser.add_argument('--data_path', type=str, default='/home/ste/Datasets',help='path to dataset')  #'/data/cappellazzo/slurp'  /data/cappellazzo/CL_SLU
-    parser.add_argument('--path_to_save_model',type=str,default='/models_SLURP_wav_SpeechBrain_intentsaug/')
-    parser.add_argument('--max_len_audio', type=int, default=112000, 
-                        help='max length for the audio signal --> it will be cut')
-    parser.add_argument('--max_len_text', type=int, default=130, 
-                        help='max length for the text sequence. This size includes the EOS or SOS token that are appended for the model computation.')
-    parser.add_argument('--download_dataset', default=False, action='store_true',
-                        help='whether to download the FSC dataset or not')
-    parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--device', type= str, default='cuda', 
-                        help='device to use for training/testing')
-    
-    
-    # Training/inference parameters.
-    
-    parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--num_workers', type=int, default=10)
-    parser.add_argument('--lr', type=float, default=5e-5, 
-                        help='learning rate (default: 5e-4)')
-    parser.add_argument("--eval_every", type=int, default=1, 
-                        help="Eval model every X epochs")
-    parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--label_smoothing', type=float, default=0., 
-                        help='Label smoothing for the CE loss')
-    parser.add_argument('--weight_decay', type=float, default=0.1)
-    parser.add_argument('--warmup_ratio', type=int, default=2)
-    
-    
-    # Encoder hyperparameters.
-    
-    parser.add_argument('--n_mels', type=int, default=768)
-    parser.add_argument('--n_seq_audio', type=int, default=400,
-                        help='Sequence length')
-    parser.add_argument('--n_hidden_audio', type=int, default=768,
-                        help='Transformer encoder hidden dim')
-    parser.add_argument('--n_head_audio', type=int, default=8,)
-    parser.add_argument('--kernel_size', type=int, default=3,)
-    parser.add_argument('--n_layer_audio', type=int, default=18)
-    parser.add_argument('--drop', type=float, default=0.1)
-    
-    # Decoder hyperparameters.
-    
-    parser.add_argument('--n_vocab', type=int, default=1000)
-    #parser.add_argument('--n_seq_text', type=int, default=16,
-    #                    help='Sequence length')
-    parser.add_argument('--n_hidden_text', type=int, default=768,
-                        help='Transformer encoder hidden dim')
-    parser.add_argument('--n_head_text', type=int, default=8,)
-    parser.add_argument('--n_layer_text', type=int, default=6)
-    parser.add_argument('--n_feedforward', type=int, default=768*4)
-    parser.add_argument('--feed_forward_expansion_factor', type=int, default=4)
-    parser.add_argument('--PAD_token', default=0)
-    
-    # Rehearsal memory.
-    
-    parser.add_argument('--memory_size', default=0, type=int,
-                        help='Total memory size in number of stored samples.')
-    parser.add_argument('--fixed_memory', default=True,
-                        help='Dont fully use memory when no all classes are seen')
-    parser.add_argument('--herding', default="random",
-                        choices=[
-                            'random',
-                            'cluster', 'barycenter',
-                        ],
-                        help='Method to herd sample for rehearsal.')
-    
-    # DISTILLATION parameters.
-    
-    parser.add_argument('--distillation-tau', default=1.0, type=float,
-                        help='Temperature for the KD')
-    parser.add_argument('--feat_space_kd', default=None, choices=[None,'only_rehe','all'])
-    parser.add_argument('--preds_space_kd', default=None, choices=[None,'only_rehe','all'])
-    
-    # Continual learning parameters.
-    
-    parser.add_argument('--increment', type=int, default=3, 
-                        help='# of classes per task/experience')
-    parser.add_argument('--initial_increment', type=int, default=3, 
-                        help='# of classes for the 1st task/experience')
-    parser.add_argument('--nb_tasks', type=int, default=6, 
-                        help='the scenario number of tasks')
-    parser.add_argument('--offline_train', default=True, action='store_true',
-                        help='whether to train in an offline fashion (i.e., no CL setting)')
-    parser.add_argument('--total_classes', type=int, default= 18, 
-                        help='The total number of classes when we train in an offline i.i.d. fashion. Set to None otherwise.')
-    parser.add_argument('--nb_classes_noCL', type=int, default= 54, 
-                        help='The total number of classes when we train in an offline i.i.d. fashion. Set to None otherwise.')
-    # WANDB parameters.
-    
-    parser.add_argument('--use_wandb', default=True, action='store_false',
-                        help='whether to track experiments with wandb')
-    parser.add_argument('--project_name', type=str, default='SLURP_experiments')
-    parser.add_argument('--exp_name', type=str, default='SLURP_SpeechBrain_BPEwav2vec_NOCTC_intents_SFaug_mymod_')
-    
-    
-    return parser
-    
-
-
-
-
-def main(args):
+@hydra.main(version_base=None, config_path='config', config_name='prompt_ast_fsc')
+def main(args) -> None:
     
     if args.use_wandb:
         
@@ -292,10 +176,10 @@ def main(args):
 
             print('Creating the CL model:')
 
-            dims = ModelDimensions(args.n_mels, args.kernel_size, args.n_hidden_audio, args.n_head_audio, args.n_layer_audio, args.n_vocab, args.n_hidden_text, n_head_text=1, n_layer_text=1, drop=args.drop, n_feedforward=768*2)
+            # dims = ModelDimensions(args.n_mels, args.kernel_size, args.n_hidden_audio, args.n_head_audio, args.n_layer_audio, args.n_vocab, args.n_hidden_text, n_head_text=1, n_layer_text=1, drop=args.drop, n_feedforward=768*2)
             
-            assert (args.n_hidden_audio % args.n_head_audio) == 0, ('Hidden dimension of encoder must be divisible by number of audio heads!')
-            assert (args.n_hidden_text % args.n_head_text) == 0, ('Hidden dimension of decoder must be divisible by number of text heads!')
+            # assert (args.n_hidden_audio % args.n_head_audio) == 0, ('Hidden dimension of encoder must be divisible by number of audio heads!')
+            # assert (args.n_hidden_text % args.n_head_text) == 0, ('Hidden dimension of decoder must be divisible by number of text heads!')
             
 
             # FIXED PROMPT ARGS: SHOUD BE ADDED TO ARGPARSE
@@ -433,7 +317,7 @@ def main(args):
                 if idx_batch % 50 == 49:
                     print(f'[{epoch + 1}, {idx_batch + 1:5d}] loss: {running_loss / 50:.3f}') 
                     running_loss=0.0
-                break
+                
                 
             ####################
             # EVALUATION PHASE #
@@ -470,7 +354,7 @@ def main(args):
                         loss = criterion(outputs, y_valid)
 
                         valid_loss +=  loss
-                        break
+                        
                     
                         
                     valid_loss /= len(valid_loader)
@@ -478,8 +362,8 @@ def main(args):
                     print(f"Valid loss: {valid_loss}")
 
 
-                    # pathh = path + f'model_SF_ep{epoch}.pth'
-                    # torch.save(model.state_dict(),pathh)
+                    pathh = path + f'model_SF_ep{epoch}.pth'
+                    torch.save(model.state_dict(),pathh)
                     print(f"Saved new model at epoch {epoch}!")
                     # best_loss = valid_loss
 
@@ -515,7 +399,7 @@ def main(args):
   
                     if args.use_wandb:
                         wandb.log({"train_loss": train_loss, "valid_loss": valid_loss, "test_loss": test_loss, "inttent_acc_test":accuracy})
-            break
+            
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
@@ -526,9 +410,8 @@ def main(args):
             
             
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser('CiCL for Spoken Language Understandig (Intent classification) on FSC: train and evaluation',
-                                    parents=[get_args_parser()])
-    args = parser.parse_args()
-
-    main(args)
+    # parser = argparse.ArgumentParser('CiCL for Spoken Language Understandig (Intent classification) on FSC: train and evaluation',
+    #                                 parents=[get_args_parser()])
+    # args = parser.parse_args()
+    main()
 
